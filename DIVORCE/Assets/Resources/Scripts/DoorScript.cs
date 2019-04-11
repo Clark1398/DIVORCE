850fc64c756c1c74ddcc5cc10f129d44845283e4
bmitch201 <33267060+bmitch201@users.noreply.github.com>
2019-04-11 12:10:05 +0100

    Fun

---
+++ DIVORCE/Assets/Resources/Scripts/DoorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Animator anim;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        anim.Play("Open");
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        anim.Play("Close");
+    }
+}
